291ed733378ef8a46e9127ed88eae769bfd0dc2f
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-10-05 17:12:13 +0300

    cleaning up

--- pages/Intro.js
+++ pages/Intro.js
@@ -1,19 +1,13 @@
-import { View, Text, StyleSheet, Image, Animated} from 'react-native'
+import { View, Text, StyleSheet, Image, Animated, ActivityIndicator } from 'react-native'
 import React, {useState, useEffect, useRef} from 'react'
 
 const Intro = ({setShowIntroPage}) => {
+  //todo: dark mode etc
 
   let scaleAnim = useRef(new Animated.Value(1)).current
   let zIndexAnim = useRef(new Animated.Value(0)).current
 
   useEffect(()=>{
-    // setTimeout(()=>{    //z index animation for kinda debugging scrollview
-    //   Animated.timing(zIndexAnim, {
-    //     toValue: -5,
-    //     duration: 50
-    //   })
-    // },2500)
-
     setTimeout(() => {    //zoom out animation for intro page
       Animated.spring(scaleAnim, {
           toValue: 0,
@@ -22,7 +16,7 @@ const Intro = ({setShowIntroPage}) => {
           useNativeDriver: false
       }).start();
     } , 2000)
-    setTimeout(()=>{setShowIntroPage(false)},2500)
+    setTimeout(()=>{setShowIntroPage(false)},2500)    //set into comp's state to false
   },[])
 
   return (
@@ -34,6 +28,9 @@ const Intro = ({setShowIntroPage}) => {
         ]}>
       <View style={styles.logoContainer}>
         <Image source={require('../assets/logo/logo-low-res.png')} style={styles.logo}/>
+      </View>
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size='large'/>
       </View>
     </Animated.View>
   )
@@ -62,5 +59,10 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     aspectRatio: 1,
     borderRadius: 200/2
+  },
+  activityIndicator: {
+    width: 50,
+    height: 50,
+    marginTop: 500
   }
 })
